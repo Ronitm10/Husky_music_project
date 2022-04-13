5fc5125e344db3c70713e178e265696362c16451
Bhargavi Rao <rao.bh@northeastern.edu>
2022-04-13 19:00:12 -0400

    model for Album.js file

--- backend/models/Album.js
+++ backend/models/Album.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose')
 
+const albumSchema = new mongoose.Schema({
+    Artist: String,
+    //list of tracks
+    albumName: String,
+    releaseYear: Date,
+    totalRunDuration: Number,
+    albumArt: String,
+    genre: String,
+});
+
 const Album = mongoose.model('Album', albumSchema);
 
 module.exports = Album
