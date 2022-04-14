b81e0dc406571913f67e6a61a2fe7912017326b9
SkandaSrinivasan <skanda@DESKTOP-SGUP9S1.localdomain>
2022-04-14 11:00:07 -0400

    user schema

--- backend/controllers/testcontroller.js
+++ backend/controllers/testcontroller.js
@@ -4,6 +4,7 @@ const Test = require('../models/Test')
 testRouter.get("/test", async (req, res) => {
     const objects = await Test.find({})
     res.status(200).json(objects)
+    
 })
 
 module.exports = testRouter
