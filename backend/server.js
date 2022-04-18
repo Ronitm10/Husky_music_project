9c6aa9c6d1fa39c608bbc6342f07b3b7cccd414a
SkandaSrinivasan <skanda@DESKTOP-SGUP9S1.localdomain>
2022-04-18 14:08:33 -0400

    added login route

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cors = require('cors')
 const express = require('express');
 const db = require('./models/db');
-const testRouter = require('./controllers/testcontroller');
+const authRouter = require('./controllers/auth');
 const userRouter = require('./controllers/usercontroller');
 
 const app = express();
@@ -10,8 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", testRouter);
-app.use("/users", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
 
 
 
