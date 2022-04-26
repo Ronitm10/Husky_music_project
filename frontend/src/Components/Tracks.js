f23bdcf2ba2a795276badcb5bf272e2cf25f9258
Ronitm10 <chougule.r@northeastern.edu>
2022-04-25 20:38:24 -0400

    Search functionality Done for Tracks Component | Added search Bar in Navbar

--- frontend/src/Components/Tracks.js
+++ frontend/src/Components/Tracks.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { Table } from 'react-bootstrap'
 import NVbar from './NVbar'
+import { InputGroup, FormControl, Button } from 'react-bootstrap'
 
 function Track() {
     const [trackList, setTrackList] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
 
     useEffect(() => {
         fetch(`http://localhost:4000/api/tracks/getTracks`).then(res => {
@@ -21,6 +23,10 @@ function Track() {
     return (
         <div>
             <NVbar />
+            <InputGroup className="mb-3" onChange={(event) => { setSearchTerm(event.target.value) }}>
+                <FormControl placeholder="Search Song" />
+                <Button variant="outline-secondary" id="button-addon2"> Search </Button>
+            </InputGroup>
             <Table striped bordered hover variant="dark">
                 <thead>
                     <tr>
@@ -31,7 +37,14 @@ function Track() {
                     </tr>
                 </thead>
                 <tbody>
-                    {trackList.map((t, i) => {
+                    {trackList.filter((val) => {
+                        if (searchTerm == '') {
+                            return val
+                        }
+                        else if (val.trackName.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                            return val
+                        }
+                    }).map((t, i) => {
                         i = i + 1;
                         return (
                             <tr className='table_row'>
