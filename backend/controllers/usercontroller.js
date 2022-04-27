07400bb2032bdad7199d718c3a2df457515f3a51
SkandaSrinivasan <srinivasan.sk@northeastern.edu>
2022-04-27 14:01:27 -0400

    changes

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -42,22 +42,7 @@ userRouter.post(
             user.password = await bcrypt.hash(password, salt)
 
             await user.save()
-
-            const payload = {
-                user: {
-                    id: user.id,
-                },
-            }
-
-            jwt.sign(
-                payload,
-                config.get('jwtSecret'),
-                { expiresIn: 360000 },
-                (err, token) => {
-                    if (err) throw err
-                    res.json({ token })
-                }
-            )
+            res.json({ msg: "User successfully created" })
 
         } catch (err) {
             console.error('oh crap user creation failed', err)
