c9ed114fdbcb0f7f8eebdafc628acc0b469d355b
SkandaSrinivasan <srinivasan.sk@northeastern.edu>
2022-04-26 21:03:55 -0400

    added jwt custom hook and login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Tracks from './Components/Tracks';
 import Albums from './Components/Albums';
+import Login from './Components/Login/Login';
+import { Container } from 'react-bootstrap';
+import NVbar from './Components/NVbar';
+import useToken from './useToken';
 
 function App() {
+
+  const { token, setToken } = useToken(); //Calling the custom hook for auth
+
+  console.log("token in app.js", token);
+  //no token, no login
+  if (!token) {
+    return (
+      <div>
+        <NVbar />
+        <Login setToken={setToken} />
+      </div>
+    )
+  }
   return (
     <div>
+      <NVbar></NVbar>
       <Router>
         <Routes>
           <Route path="/tracks" element={<Tracks />}></Route>
