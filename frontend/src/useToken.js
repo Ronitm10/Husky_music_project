e25e0ff0ef484dede959cadf7513de1db91f0f8f
SkandaSrinivasan <srinivasan.sk@northeastern.edu>
2022-04-26 21:03:55 -0400

    added jwt custom hook and login form

---
+++ frontend/src/useToken.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const useToken = () => {
+    const getToken = () => {
+        const tokenString = sessionStorage.getItem('token');
+        const userToken = JSON.parse(tokenString);
+        return userToken?.id
+    }
+
+    const [token, setToken] = useState(getToken());
+
+    //set token in session storage
+    const saveToken = userToken => {
+        sessionStorage.setItem('token', JSON.stringify(userToken));
+        setToken(userToken)
+    }
+    return {
+        setToken: saveToken,
+        token
+    }
+}
+
+export default useToken
