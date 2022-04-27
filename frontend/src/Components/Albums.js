48f230ae38e25069f6aeae5f04c25900d3ab3b7b
Bhargavi Rao <rao.bh@northeastern.edu>
2022-04-26 22:01:48 -0400

    Added code to Search for album name

--- frontend/src/Components/Albums.js
+++ frontend/src/Components/Albums.js
@@ -1,44 +1,71 @@
-import './Album.css'
-import React, { useState, useEffect } from 'react'
-import { Card,Button,Container, CardGroup } from 'react-bootstrap'
-import  Image from "../Components/Shakira.png"
+import "./Album.css";
+import React, { useState, useEffect } from "react";
+import NVbar from "./NVbar";
+import { Nav, Navbar, Card, Container, InputGroup, FormControl, Button } from "react-bootstrap";
+import Image from "../Components/Shakira.png";
+import Track from "./Tracks";
+
 function Album() {
-    
-const [albumList, setAlbumList] = useState([])
+  const [albumList, setAlbumList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("")
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/api/albums/getAll`).then(res => {
-            res.json().then(result => {
-                let albums = result;
-                setAlbumList(albums)
-                console.log(albums)
-            })
-        })
-            .catch(err => {
-                console.error("Weather fetch failed: ", err);
-            })
-    }, [])
 
-            return (
-                albumList.map((t) => {
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/albums/getAll`)
+      .then((res) => {
+        res.json().then((result) => {
+          let albums = result;
+          setAlbumList(albums);
+          console.log(albums);
+        });
+      })
+      .catch((err) => {
+        console.error("Weather fetch failed: ", err);
+      });
+  }, []);
+
+
+  return (
+    <div>
+        <NVbar />
+        <InputGroup className="mb-3" onChange={(event) => { setSearchTerm(event.target.value) }}>
+            <FormControl placeholder="Search Album" />
+            <Button variant="outline-secondary" id="button-addon2"> Search </Button>
+        </InputGroup>
+       
+                {albumList.filter((val) => {
+                    if (searchTerm == '') {
+                        return val
+                    }
+                    else if (val.albumName.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                        return val
+                    }
+                }).map((t, i) => {
+                    i = i + 1;
                     return (
-                        <div class='outer'>
-                        <Container fluid>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={Image} />
-                                <Card.Body>
-                                    <Card.Title class='title'>{t.albumName}</Card.Title>
-                                    <Card.Text class='text'>{t.genre}</Card.Text>
-                                    <Button variant="primary">Click here to view Tracks</Button>
-                                </Card.Body>
-                            </Card>
-                            </Container>
-                            
-                        </div>
+
+                      <div class="outer">
+                      <Container fluid>
+                          <Card style={{ width: "18rem" }}>
+                              <Card.Img variant="top" src={Image} />
+                              <Card.Body>
+                                  <Card.Title class="title">{t.albumName}</Card.Title>
+                                  <Card.Text class="text">{t.genre}</Card.Text>
+                                  <Button variant="primary" onClick={()=> Track(
+                                    <Track
+                                        alName ={t.albumName} 
+                                    />
+                                  )}>Click here to view Tracks</Button>
+                              </Card.Body>
+                          </Card>
+                      </Container>
+                </div>
                     )
                 }
-                )
-            )
-            }
+                )}
+    </div>
+)
 
-export default Album
+}
+
+export default Album;
