ded8728d7de2d7e0b47d3ddba54e75158b2c1453
msahilm <mattoo.s@northeastern.edu>
2022-04-28 14:58:18 -0400

    all artist view page modified

--- frontend/src/Components/Artist/AllArtist.js
+++ frontend/src/Components/Artist/AllArtist.js
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { InputGroup, FormControl, Button, Table } from 'react-bootstrap'
 import profileImg from "../../assets/Shawn-Mendes.webp";
+import './ArtistProfile.css';
 
 
 function AllArtist() {
    
 const[searchTerm, setSearchTerm]= useState('');
 const artistList = [
-    { album: "Album 1", releasedin: "2020",albumDuration: "120", link: profileImg},
-    { album: "Album 2", releasedin: "2019",albumDuration: "150", link: profileImg },
-    {album: "Album 3", releasedin: "2005",albumDuration: "10", link: profileImg},
-    {album: "Album 4", releasedin: "1997",albumDuration: "90", link: profileImg}
+    { album: "Weeknd", releasedin: "2020",albumDuration: "120", link: profileImg},
+    { album: "Kishore kumar", releasedin: "2019",albumDuration: "150", link: profileImg },
+    {album: "Metallica", releasedin: "2005",albumDuration: "10", link: profileImg},
+    {album: "Nirvana", releasedin: "1997",albumDuration: "90", link: profileImg}
   ];
    
     return (
         <div>
 
             <InputGroup className="mb-3" onChange={(event) => { setSearchTerm(event.target.value) }}>
-                <FormControl placeholder="Search Song" />
-                <Button variant="outline-secondary" id="button-addon2"> Search </Button>
+                <FormControl placeholder="Search your favourite artist" />
+                <Button variant="outline-secondary" id="button-addon2"> Find </Button>
             </InputGroup>
             <Table striped bordered hover variant="dark">
                 <thead>
                     <tr>
-                        <th>Album art</th>
-                        <th><i>Album</i></th>
-                        <th><i>Year</i></th>
-                        <th><i>Duration</i></th>
+                        <th>Count</th>
+                        <th><i>Artist name</i></th>
+                        
                     </tr>
                 </thead>
                 <tbody>
@@ -41,10 +41,9 @@ const artistList = [
                         i = i + 1;
                         return (
                             <tr className='table_row'>
-                                <td>{i}</td>
+                                <td class="albumArtContainer"><img class="albumArt" src={t.link}></img></td>
                                 <td>{t.album}</td>
-                                <td>{t.releasedin}</td>
-                                <td>{t.albumDuration}</td>
+                               
                             </tr>
                         )
                     }
