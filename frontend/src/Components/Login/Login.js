66883d2589907fdfd1c2a946b8133fd7363719e1
Ronitm10 <chougule.r@northeastern.edu>
2022-04-30 00:14:55 -0400

    Home page layout and background changes

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom'
 import useToken from '../../useToken'
 import { getToken } from '../../helpers'
 import husky from '../../assets/app-logo.svg.png'
+import LoginHomePage from './LoginHomePage'
 
 const axios = require('axios')
 
@@ -46,49 +47,50 @@ const Login = ({ setToken }) => {
     }
     return (
         <div class="mainCon">
-       
-        <Container className="login">
-           
-           
-            <div className="d-flex flex-row mb-3 justify-content-around">
-                
-        <div className="p-2">
-        <figure>
-        <img class="logo" src={husky}></img>
-    <figcaption class="brand-name"> Husky Music</figcaption>
-</figure> 
-       
-        </div>
 
-        <div className="p-2">
-            <div class="login-panel">
-             <Form className="login-wrapper" onSubmit={handleSubmit}>
-                <h1 style={{padding: "20px"}}>Login</h1>
-                <Form.Group className="mb-3" style={{ width: "300px", marginLeft: "inherit", marginRight: "inherit"}} controlId="formBasicEmail">
-                    <Form.Label style={{padding: "20px", textAlign:"center"}}>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" onChange={e => setUserName(e.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3" style={{ width: "300px", marginLeft: "inherit", marginRight: "inherit"}} controlId="formBasicPassword">
-                    <Form.Label style={{ padding: "20px"}}>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-                </Form.Group>
-                <div  style={{ padding: "40px"}}>
-                <Button class="submit" variant="outline-success" type="submit">
-                    Log in
-                </Button>
-                {
-                    hasError ?
-                        <Alert style={{ margin: "5%" }} variant='danger'>
-                            {error}
-                        </Alert> : <></>
-                }
+            <Container className="login">
+
+
+                <div className="d-flex flex-row mb-3 justify-content-around">
+
+                    <div className="p-2">
+                        <figure>
+                            <img class="logo" src={husky} alt=""></img>
+                            <figcaption class="brand-name"> Husky Music</figcaption>
+                        </figure>
+
+                    </div>
+
+                    <div className="p-2">
+                        <div class="login-panel">
+                            <Form className="login-wrapper" onSubmit={handleSubmit}>
+                                <h1 style={{ padding: "20px" }}>Login</h1>
+                                <Form.Group className="mb-3" style={{ width: "300px", marginLeft: "inherit", marginRight: "inherit" }} controlId="formBasicEmail">
+                                    <Form.Label style={{ padding: "20px", textAlign: "center" }}>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" onChange={e => setUserName(e.target.value)} />
+                                </Form.Group>
+                                <Form.Group className="mb-3" style={{ width: "300px", marginLeft: "inherit", marginRight: "inherit" }} controlId="formBasicPassword">
+                                    <Form.Label style={{ padding: "20px" }}>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+                                </Form.Group>
+                                <div style={{ padding: "40px" }}>
+                                    <Button class="submit" variant="outline-success" type="submit">
+                                        Log in
+                                    </Button>
+                                    {
+                                        hasError ?
+                                            <Alert style={{ margin: "5%" }} variant='danger'>
+                                                {error}
+                                            </Alert> : <></>
+                                    }
+                                </div>
+                            </Form>
+                        </div>
+                    </div>
                 </div>
-            </Form>
-            </div>
-        </div>
-        </div>
-       
-        </Container>
+
+            </Container>
+            <LoginHomePage />
         </div>
     )
 }
